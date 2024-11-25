0436d5105fe93b664412647455173489acd8d7fd
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-11-25 15:30:27 +0100

    ci:add planet model

--- src/main/java/dev/nyanchuk/Planet.java
+++ src/main/java/dev/nyanchuk/Planet.java
@@ -5,18 +5,19 @@ public class Planet {
     // Attributes
     private String name = null;
     private int numberOfSatellite = 0;
-    private double massInKg = 0.0; 
+    private double massInKg = 0.0;
     private double volumeIn3Km = 0.0;
-    private int diameterInKm = 0; 
+    private int diameterInKm = 0;
     private int averageDistanceToSunInMillionKm = 0;
     private PlanetType type = PlanetType.TERRESTRIAL;
     private boolean isVisible = false;
-
-    
+    private double orbitalPeriodInYears = 0.0;
+    private int rotationPeriodInDays = 0;
 
     // Constructor to initialize the attributes
     public Planet(String name, int numberOfSatellite, double massInKg, double volumeIn3Km, int diameterInKm,
-                  int averageDistanceToSunInMillionKm, PlanetType type, boolean isVisible) {
+            int averageDistanceToSunInMillionKm, PlanetType type, boolean isVisible,
+            double orbitalPeriodInYears, int rotationPeriodInDays) {
         this.name = name;
         this.numberOfSatellite = numberOfSatellite;
         this.massInKg = massInKg;
@@ -25,10 +26,13 @@ public class Planet {
         this.averageDistanceToSunInMillionKm = averageDistanceToSunInMillionKm;
         this.type = type;
         this.isVisible = isVisible;
+        this.orbitalPeriodInYears = orbitalPeriodInYears;
+        this.rotationPeriodInDays = rotationPeriodInDays;
     }
 
     // Default constructor
-    public Planet() {}
+    public Planet() {
+    }
 
     // Getters and Setters
     public String getName() {
@@ -39,43 +43,43 @@ public class Planet {
         this.name = name;
     }
 
-    public int getnumberOfSatellite() {
+    public int getNumberOfSatellite() {
         return numberOfSatellite;
     }
 
-    public void setnumberOfSatellite(int numberOfSatellite) {
+    public void setNumberOfSatellite(int numberOfSatellite) {
         this.numberOfSatellite = numberOfSatellite;
     }
 
-    public double getmassInKg() {
+    public double getMassInKg() {
         return massInKg;
     }
 
-    public void setmassInKg(double massInKg) {
+    public void setMassInKg(double massInKg) {
         this.massInKg = massInKg;
     }
 
-    public double getvolumeIn3Km() {
+    public double getVolumeIn3Km() {
         return volumeIn3Km;
     }
 
-    public void setvolumeIn3Km(double volumeIn3Km) {
+    public void setVolumeIn3Km(double volumeIn3Km) {
         this.volumeIn3Km = volumeIn3Km;
     }
 
-    public int getdiameterInKm() {
+    public int getDiameterInKm() {
         return diameterInKm;
     }
 
-    public void setdiameterInKm(int diameterInKm) {
+    public void setDiameterInKm(int diameterInKm) {
         this.diameterInKm = diameterInKm;
     }
 
-    public int getaverageDistanceToSunInMillionKm() {
+    public int getAverageDistanceToSunInMillionKm() {
         return averageDistanceToSunInMillionKm;
     }
 
-    public void setaverageDistanceToSunInMillionKm(int averageDistanceToSunInMillionKm) {
+    public void setAverageDistanceToSunInMillionKm(int averageDistanceToSunInMillionKm) {
         this.averageDistanceToSunInMillionKm = averageDistanceToSunInMillionKm;
     }
 
@@ -91,41 +95,23 @@ public class Planet {
         return isVisible;
     }
 
-    public void setisVisible(boolean isVisible) {
+    public void setIsVisible(boolean isVisible) {
         this.isVisible = isVisible;
     }
 
-    // Method to calculate the density of the planet
-    public double calculateDensity() {
-        if (volumeIn3Km == 0) {
-            return 0;
-        }
-        return massInKg / volumeIn3Km;
+    public double getOrbitalPeriodInYears() {
+        return orbitalPeriodInYears;
     }
 
-    // Method to check if a planet is an outer planet
-    public boolean isOuterPlanet() {
-        // Outer planet is beyond the asteroid belt (between 2.1 and 3.4 AU)
-        return averageDistanceToSunInMillionKm > 2.1 && averageDistanceToSunInMillionKm < 3.4;
+    public void setOrbitalPeriodInYears(double orbitalPeriodInYears) {
+        this.orbitalPeriodInYears = orbitalPeriodInYears;
     }
 
-    // Method to display the planet's details
-    public void printDetails() {
-        System.out.println("Planet Name: " + name);
-        System.out.println("Number of Satellites: " + numberOfSatellite);
-        System.out.println("Mass: " + massInKg + " kg");
-        System.out.println("Volume: " + volumeIn3Km + " km³");
-        System.out.println("Diameter: " + diameterInKm + " km");
-        System.out.println("Average Distance to Sun: " + averageDistanceToSunInMillionKm + " million km");
-        System.out.println("Planet Type: " + type);
-        System.out.println("Observable: " + isVisible);
+    public int getRotationPeriodInDays() {
+        return rotationPeriodInDays;
     }
 
-    // Main method to test the class
-    public static void main(String[] args) {
-        Planet earth = new Planet("Earth", 1, 5.972E24, 1.08321E12, 12742, 1, PlanetType.TERRESTRIAL, true);
-        earth.printDetails();
-        System.out.println("Density: " + earth.calculateDensity() + " kg/km³");
-        System.out.println("Is Outer Planet: " + earth.isOuterPlanet());
+    public void setRotationPeriodInDays(int rotationPeriodInDays) {
+        this.rotationPeriodInDays = rotationPeriodInDays;
     }
 }
